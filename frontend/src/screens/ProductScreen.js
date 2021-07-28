f6aac0d440d6ba1d77c3663141d88490bd2d6fda
fapjsc <p82szh@gmail.com>
2021-07-28 22:53:45 +0800

    create product detail page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+// Style
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+
+// Component
+import Rating from '../components/Rating';
+
+// Dummy Data
+import products from '../products';
+
+const ProductScreen = ({ match }) => {
+  const product = products.find(el => el._id === match.params.id);
+  console.log(product);
+
+  return (
+    <>
+      <Row className="">
+        <Col>
+          <Link to="/" className="btn btn-light my-3">
+            Go Back
+          </Link>
+        </Col>
+      </Row>
+
+      <Row className="">
+        <Col md={6} className="">
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3} className="">
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews}review`} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>State:</Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item className="d-grid">
+                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
