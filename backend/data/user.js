0ae77f88ce28fdd9f4a450f11ed7a6f75f25a495
fapjsc <p82szh@gmail.com>
2021-07-31 10:10:04 +0800

    新增數據庫數據模型

---
+++ backend/data/user.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    name: 'Admin',
+    email: 'admin@example.com',
+    password: xxx,
+    idAdmin: true,
+  },
+  {
+    name: 'John',
+    email: 'john@example.com',
+    password: xxx,
+    idAdmin: false,
+  },
+  {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: xxx,
+    idAdmin: false,
+  },
+];
+
+export default users;
